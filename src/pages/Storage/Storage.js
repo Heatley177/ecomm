da18546c8cf61ae8f25f6c916edd9d583053b616
Heatley177 <heatleydesign@gmail.com>
2023-07-18 13:28:14 -0400

    mocked Loading Skeleton to Storage page CommonBtn

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
 import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
 import CommonButton from '../../components/common/CommonButton/CommonButton';
+import Loading from '../../components/common/Loading/Loading';
 const Storage = () => {
 
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] =useState(true);
 
   const handleClick = () => {
     setOpen(true);
@@ -18,14 +20,32 @@ const Storage = () => {
     setOpen(false);
   };
 
+  useEffect(() => {
+      const timer = setTimeout(() => {
+        setLoading(false)
+      }, 2000);
+      return () => clearTimeout(timer);
+  }, []);
+
   return (
     <GridWrapper>
-      <CommonButton 
-            variant="contained" 
-            onClick={handleClick}
-        >
-            Open success snackbar
-      </CommonButton>
+      { loading ? 
+        <Loading>
+          <CommonButton 
+                variant="contained" 
+                onClick={handleClick}
+            >
+                Open success snackbar
+          </CommonButton>
+        </Loading>
+        :
+        <CommonButton 
+              variant="contained" 
+              onClick={handleClick}
+          >
+              Open success snackbar
+        </CommonButton>
+      }
       <BasicSnackbar 
         open={open}
         onClose={handleClose}
