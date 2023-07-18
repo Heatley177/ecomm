171d88bbe68d20fbd8161aeddba7d0c80ec072bb
Heatley177 <heatleydesign@gmail.com>
2023-07-18 16:02:29 -0400

    Removed Loading Skeleton & added DataGrid/Table

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,56 +1,32 @@
 import React, {useEffect, useState} from 'react'
 import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
+import UserTable from '../../components/common/UserTable/UserTable';
+import BasicCard from '../../components/common/BasicCard/BasicCard';
 import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
-import CommonButton from '../../components/common/CommonButton/CommonButton';
-import Loading from '../../components/common/Loading/Loading';
+import { set } from 'react-hook-form';
+
+
 const Storage = () => {
-
   const [open, setOpen] = useState(false);
-  const [loading, setLoading] =useState(true);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
 
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
-
     setOpen(false);
   };
 
-  useEffect(() => {
-      const timer = setTimeout(() => {
-        setLoading(false)
-      }, 2000);
-      return () => clearTimeout(timer);
-  }, []);
 
   return (
     <GridWrapper>
-      { loading ? 
-        <Loading>
-          <CommonButton 
-                variant="contained" 
-                onClick={handleClick}
-            >
-                Open success snackbar
-          </CommonButton>
-        </Loading>
-        :
-        <CommonButton 
-              variant="contained" 
-              onClick={handleClick}
-          >
-              Open success snackbar
-        </CommonButton>
-      }
+      <BasicCard 
+        content={<UserTable onError={() => setOpen(true) } />}
+      />
       <BasicSnackbar 
         open={open}
+        severity='error'
+        message="Data couldn't be fetched"
         onClose={handleClose}
-        severity='success'
-        message='Success!'
       />
     </GridWrapper>
   )
